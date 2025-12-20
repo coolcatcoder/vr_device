c03a774d5764b202d2d8ca3329826ade15d26845
Katt <51190960+coolcatcoder@users.noreply.github.com>
2025-12-20 16:18:01 +0800

    slight improvements

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,14 @@
 
 use std::{ffi::{CStr, c_char, c_int, c_void}, ptr::null_mut};
 
-#[allow(non_upper_case_globals)]
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(unused)]
-#[allow(clippy::pedantic)]
-mod raw_bindings {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
+// bindgen wrapper.hpp -o ./src/new_bindings.rs --vtable-generation --allowlist-item "vr::IServerTrackedDeviceProvider" --allowlist-item "vr::IServerTrackedDeviceProvider_Version"
+mod bindings;
 
 #[unsafe(no_mangle)]
 extern "C" fn HmdDriverFactory(interface_name: *const c_char, return_code: *mut c_int) -> *mut c_void
 {
     let interface_name = unsafe { CStr::from_ptr(interface_name) };
-    if CStr::from_bytes_with_nul(raw_bindings::vr_IServerTrackedDeviceProvider_Version).expect("Valid CStr.") == interface_name {
+    if bindings::SERVER_TRACKED_DEVICE_PROVIDER_VERSION == interface_name {
         // let device_provider_vtable = Box::new(raw_bindings::vr_IServerTrackedDeviceProvider__bindgen_vtable {
         //     vr_IServerTrackedDeviceProvider_
         // });
@@ -27,7 +21,28 @@ extern "C" fn HmdDriverFactory(interface_name: *const c_char, return_code: *mut 
     }
     
     unsafe {
-        *return_code = raw_bindings::vr_EVRInitError_VRInitError_Init_InterfaceNotFound as c_int;
+        *return_code = bindings::VrInitError::InitInterfaceNotFound as c_int;
         null_mut()
     }
 }
+
+unsafe extern "C" fn init(
+        this: *mut bindings::ServerTrackedDeviceProvider,
+        context: *mut bindings::vr_IVRDriverContext,
+    ) -> bindings::VrInitError {
+        todo!()
+    }
+
+static DEVICE_PROVIDER_VTABLE: bindings::ServerTrackedDeviceProviderVtable = bindings::ServerTrackedDeviceProviderVtable {
+    init,
+    cleanup: todo!(),
+    get_interface_versions: todo!(),
+    run_frame: todo!(),
+    should_block_standby_mode: todo!(),
+    enter_standby: todo!(),
+    leave_standby: todo!(),
+};
+
+// static device_provider = bindings::ServerTrackedDeviceProvider {
+//     v
+// }
